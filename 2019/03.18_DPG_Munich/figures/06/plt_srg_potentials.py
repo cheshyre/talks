34511bcf6932ba0fc57ae428ef2310987bbc26c4
Matthias Heinz <heinz.matthias.3@gmail.com>
2019-05-17 16:12:36 +0200

    06 - Add comments to scripts to generate figures

--- 2019/03.18_DPG_Munich/figures/06/plt_srg_potentials.py
+++ 2019/03.18_DPG_Munich/figures/06/plt_srg_potentials.py
@@ -1,30 +1,42 @@
-import json
+"""Script to generate plot of series of potentials in SRG run."""
 import glob
-import scipy.interpolate
-import matplotlib.pyplot as plt
-import numpy as np
+import json
+
 import matplotlib
-
+import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
+import numpy as np
+import scipy.interpolate
 
-matplotlib.rcParams['text.latex.preamble'] = [
-       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
-       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
-       r'\usepackage{helvet}',    # set the normal font here
-       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
-       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
+# matplotlib font configurations
+matplotlib.rcParams["text.latex.preamble"] = [
+    # i need upright \micro symbols, but you need...
+    r"\usepackage{siunitx}",
+    # ...this to force siunitx to actually use your fonts
+    r"\sisetup{detect-all}",
+    # set the normal font here
+    r"\usepackage{helvet}",
+    # load up the sansmath so that math -> helvet
+    r"\usepackage{sansmath}",
+    # <- tricky! -- gotta actually tell tex to use!
+    r"\sansmath",
 ]
-plt.rc('text', usetex=True)
 
+# Use tex for matplotlib
+plt.rc("text", usetex=True)
 
-kmax = 12.0
-lambdas = [50, 5.0, 3.0, 2.0, 1.5]
-#  path = 'data/BlockDiag_soft_1.5_0.2_*/{}'
-path = 'data/BandDiag_soft_1.25_0.2_*/{}'
-path = 'data/T_rel_*/{}'
+# Set path for data
+# pylint: disable=C0103
 path = './fig_data/{}'
+
+# Set figure height
 fig_height = 1.3
 
+# Set maximum momentum to show
+kmax = 12.0
+
+# Set lambdas to show
+lambdas = [50, 5.0, 3.0, 2.0, 1.5]
 
 # Get file
 file = glob.glob(path.format('*pot*.json'))
@@ -48,35 +60,42 @@ grid = ImageGrid(figure, 111, nrows_ncols=(1, len(lambdas)), axes_pad=0.0,
                  share_all=False, label_mode='L', cbar_location='right',
                  cbar_mode='single')
 
+# Iterate over lambdas
 for i, lam in enumerate(lambdas):
+    # Read in potential
     potential = np.array(data['data'][str(lam)])
 
+    # Interpolate potential
     interp = scipy.interpolate.RectBivariateSpline(data_nodes, data_nodes,
                                                    potential)
-
     potential = interp(nodes, nodes)
 
+    # Plot potential matrix on corresponding axis in grid
     ax = grid[i]
-
     im = ax.matshow(potential, extent=[0.0, kmax, kmax, 0.0],
                     vmax=2, vmin=-2)
-    #  if i != 0:
-        #  print(labels[-1])
-        #  #  labels[-1] = ''
-        #  print(labels)
+
+    # Set ticks for axes for this axis
     ax.set_xticks([0, 5, 10])
     ax.set_yticks([0, 5, 10])
+
+    # Set xlabel on 3rd grid
     if i == 2:
         ax.set_xlabel(r"$k'$ (fm$^{-1}$)", labelpad=8)
         ax.xaxis.set_label_position('top')
+
+    # Set ylabel on first grid
     if i == 0:
         ax.set_ylabel(r"$k$ (fm$^{-1}$)")
+
+    # Add label for lambda value
     unit = r' fm$^{-1}$'
     if lam == 50:
         lam = r'\infty'
         unit = ''
     ax.text(1, 11, '$\\lambda = {}${}'.format(lam, unit))
 
+    # Move xticks to top
     ax.tick_params(
         axis='x',
         which='both',
@@ -84,6 +103,7 @@ for i, lam in enumerate(lambdas):
         top=True,
         labelbottom=False,
     )
+    # Disable yticks for grids after the left most
     if i != 0:
         ax.tick_params(
             axis='y',
@@ -91,15 +111,10 @@ for i, lam in enumerate(lambdas):
             left=False,
         )
 
-#  for i, ax in enumerate(grid.axes_all):
-#      labels = ax.get_xticklabels()
-#      labels[-1] = ''
-#      if
-#      ax.set_xticklabels(labels)
-
-
+# Set colorbar
 grid.cbar_axes[0].colorbar(im)
 
+# Set colorbar labels
 for i, cax in enumerate(grid.cbar_axes):
     cax.set_yticks([-2, 0, 2])
     labels = [item.get_text() for item in cax.get_yticklabels()]
@@ -107,6 +122,7 @@ for i, cax in enumerate(grid.cbar_axes):
     labels[-1] = r'2 (MeV)'
     cax.set_yticklabels(labels)
 
+# Set xticks to top
 plt.tick_params(
     axis='x',
     which='both',
@@ -114,10 +130,16 @@ plt.tick_params(
     top=True,
     labelbottom=False,
 )
+
+# Adjust margins
 #  plt.gcf().subplots_adjust(left=0.075)
 plt.gcf().subplots_adjust(right=0.85)
 plt.gcf().subplots_adjust(top=0.82)
 plt.gcf().subplots_adjust(bottom=-0.08)
+
+# Set size of plot
 plt.gcf().set_size_inches(5.0 * fig_height, 1.2 * fig_height)
+
+# Save as PDF
 plt.savefig('potentials.pdf')
 
