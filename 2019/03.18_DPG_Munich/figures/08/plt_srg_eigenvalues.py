bd905797acaa52526ffbc59ff9aa731e35f9ed50
Matthias Heinz <heinz.matthias.3@gmail.com>
2019-05-17 16:42:37 +0200

    08 - Add comments and formatting, new plots

--- 2019/03.18_DPG_Munich/figures/08/plt_srg_eigenvalues.py
+++ 2019/03.18_DPG_Munich/figures/08/plt_srg_eigenvalues.py
@@ -1,79 +1,100 @@
-import json
-import matplotlib.pyplot as plt
-import matplotlib
+"""Script to plot 3-body ground state energies for the SRG run."""
 import glob
-import numpy as np
+import json
 
-matplotlib.rcParams['text.latex.preamble'] = [
-       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
-       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
-       r'\usepackage{helvet}',    # set the normal font here
-       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
-       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
+import matplotlib
+import matplotlib.pyplot as plt
+
+# matplotlib font configurations
+matplotlib.rcParams["text.latex.preamble"] = [
+    # i need upright \micro symbols, but you need...
+    r"\usepackage{siunitx}",
+    # ...this to force siunitx to actually use your fonts
+    r"\sisetup{detect-all}",
+    # set the normal font here
+    r"\usepackage{helvet}",
+    # load up the sansmath so that math -> helvet
+    r"\usepackage{sansmath}",
+    # <- tricky! -- gotta actually tell tex to use!
+    r"\sansmath",
 ]
-plt.rc('text', usetex=True)
-#  plt.rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-#  plt.rc('font', family='sans-serif')
+
+# Use tex for matplotlib
+plt.rc("text", usetex=True)
+
+# Set path for data
+# pylint: disable=C0103
+path = "./fig_data/{}"
+
+# Height for figure
 fig_height = 1.3
 
-
-#  path = 'data/T_rel*/{}'
-path = './fig_data/{}'
-
-file = glob.glob(path.format('t_rel*eigen*.json'))[0]
-
-with open(file) as f:
-    data = json.load(f)
-
-lambdas = [float(key) for key in data]
-vals = [data[key]['ho3'] for key in data]
-
+# Create figure
 fig1, ax1 = plt.subplots()
 
-ax1.plot(lambdas, vals, 'r.', label=r'$T_{rel}$')
-ax1.plot(lambdas, vals, 'r-')
-
-file = glob.glob(path.format('BlD*eigen*.json'))[0]
-
+# Load file
+file = glob.glob(path.format("t_rel*eigen*.json"))[0]
 with open(file) as f:
     data = json.load(f)
 
+# Get lambdas and values
 lambdas = [float(key) for key in data]
-vals = [data[key]['ho3'] for key in data]
+vals = [data[key]["ho3"] for key in data]
 
+# Plot data, once for points, one for the line
+ax1.plot(lambdas, vals, "r.", label=r"$T_{rel}$")
+ax1.plot(lambdas, vals, "r-")
 
-ax1.plot(lambdas, vals, 'b.', label=r'Block')
-ax1.plot(lambdas, vals, 'b-')
-
-file = glob.glob(path.format('BaD*eigen*.json'))[0]
-
+# Load file
+file = glob.glob(path.format("BlD*eigen*.json"))[0]
 with open(file) as f:
     data = json.load(f)
 
+# Get lambdas and values
 lambdas = [float(key) for key in data]
-vals = [data[key]['ho3'] for key in data]
+vals = [data[key]["ho3"] for key in data]
 
+# Plot data, once for points, one for the line
+ax1.plot(lambdas, vals, "b.", label=r"Block")
+ax1.plot(lambdas, vals, "b-")
 
-ax1.plot(lambdas, vals, 'm.', label=r'Band')
-ax1.plot(lambdas, vals, 'm-')
+# Load file
+file = glob.glob(path.format("BaD*eigen*.json"))[0]
+with open(file) as f:
+    data = json.load(f)
 
+# Get lambdas and values
+lambdas = [float(key) for key in data]
+vals = [data[key]["ho3"] for key in data]
 
-ax1.set_xscale('log')
+# Plot data, once for points, one for the line
+ax1.plot(lambdas, vals, "m.", label=r"Band")
+ax1.plot(lambdas, vals, "m-")
+
+# Set x-axis scale and ticks
+ax1.set_xscale("log")
 ax1.set_xticks([1, 2, 5, 10, 20, 50])
 ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
-#  plt.title('\"Triton\" Eigenvalue Running')
-plt.xlabel(r'$\lambda$ (fm$^{-1}$)')
-plt.ylabel(r'$E_3$ (MeV)')
-plt.ylim(-3.0, -2.5)
-plt.legend(loc='lower right', fontsize='small')
-#  plt.ylabel('E_3')
+# Set axis labels and title
+plt.xlabel(r"$\lambda$ (fm$^{-1}$)")
+plt.ylabel(r"$E_3$ (MeV)")
+plt.title("3-Body Binding Energy", x=0.4, y=1.05)
 
-#  plt.gcf().set_size_inches(4.4, 3.8)
-plt.title('3-Body Binding Energy', x=0.4, y=1.05)
+# Set y-axis limits
+plt.ylim(-3.0, -2.5)
+
+# Enable legend
+plt.legend(loc="lower right", fontsize="small")
+
+# Adjust margins
 plt.gcf().subplots_adjust(left=0.30)
 plt.gcf().subplots_adjust(right=0.99)
 plt.gcf().subplots_adjust(top=0.85)
 plt.gcf().subplots_adjust(bottom=0.20)
+
+# Set size of plot
 plt.gcf().set_size_inches(1.6 * fig_height, 1.6 * fig_height)
-plt.savefig('eigenvalues.pdf')
+
+# Save as PDF
+plt.savefig("eigenvalues.pdf")
